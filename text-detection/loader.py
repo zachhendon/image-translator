55198c9c0fdd26541f7d4019907dbea531e05e5d
Zach Hendon <zachendo3@gmail.com>
2024-12-08 17:54:46 -0600

    Synthtext dataset processing and loading

--- text-detection/loader.py
+++ text-detection/loader.py
@@ -8,41 +8,48 @@ from glob import glob
 import math
 
 
-class ICDAR2015Dataset(Dataset):
-    def __init__(self, datadir, train):
+class FastDataset(Dataset):
+    def __init__(self, datadir, train, ignore):
         self.datadir = datadir
         self.train = train
+        self.ignore = ignore
         self.num_images = len(glob(f"{datadir}/images/*.jpg"))
 
         self.normalize = K.Normalize(
             mean=torch.tensor([0.485, 0.456, 0.406]),
             std=torch.tensor([0.229, 0.224, 0.225]),
         )
+
+        self.data_keys = ["image", "keypoints", "keypoints"]
+        if ignore:
+            self.data_keys += ["keypoints", "keypoints"]
         self.aug = K.AugmentationSequential(
             K.RandomHorizontalFlip(p=0.5),
             K.RandomVerticalFlip(p=0.5),
-            # K.RandomAffine(
-            #     degrees=(-20, 20),
-            #     translate=(0.2, 0.2),
-            #     shear=(-20, 20),
-            #     scale=(0.8, 1.2),
-            #     p=1.0,
-            # ),
-            # K.RandomPerspective(distortion_scale=0.3, p=0.3),
+            K.RandomAffine(
+                degrees=(-20, 20),
+                translate=(0.2, 0.2),
+                shear=(-20, 20),
+                scale=(0.8, 1.2),
+                p=1.0,
+            ),
+            K.RandomPerspective(distortion_scale=0.3, p=0.3),
             K.RandomCrop((640, 640)),
             # K.ColorJitter(0.1, 0.1, 0.1, 0.1),
-            # K.RandomGaussianBlur((3, 3), (0.1, 2.0)),
-            # K.RandomSharpness(0.5),
-            # K.RandomGaussianNoise(mean=0, std=0.05, p=1),
-            data_keys=["image", "keypoints", "keypoints", "keypoints", "keypoints"],
+            K.RandomGaussianBlur((3, 3), (0.1, 2.0)),
+            K.RandomSharpness(0.5),
+            # K.RandomGaussianNoise(mean=0, std=1.00, p=1),
+            data_keys=self.data_keys,
             same_on_batch=False,
         )
-        # else:
-        #     self.aug = K.AugmentationSequential(
-        #         K.SmallestMaxSize(640),
-        #         data_keys=["image", "keypoints", "keypoints", "keypoints", "keypoints"],
-        #         same_on_batch=True,
-        #     )
+        self.upscale = K.AugmentationSequential(
+            K.SmallestMaxSize(640),
+            data_keys=self.data_keys,
+        )
+        self.resize = K.AugmentationSequential(
+            K.Resize((640, 640)),
+            data_keys=self.data_keys,
+        )
 
     def __len__(self):
         return self.num_images
@@ -104,6 +111,64 @@ class ICDAR2015Dataset(Dataset):
             l = r
         return batch_kernel_masks, batch_text_masks
 
+    def apply_augmentations(self, images, bboxes, min_bboxes, aug):
+        if type(images) == list:
+            images_aug = []
+            bboxes_aug = []
+            min_bboxes_aug = []
+
+            for image, bbox, min_bbox in zip(images, bboxes, min_bboxes):
+                image, bbox, min_bbox = aug(
+                    image.unsqueeze(0), bbox.unsqueeze(0), min_bbox.unsqueeze(0)
+                )
+                images_aug.append(image.squeeze(0))
+                bboxes_aug.append(bbox.squeeze(0))
+                min_bboxes_aug.append(min_bbox.squeeze(0))
+        else:
+            images_aug, bboxes_aug, min_bboxes_aug = aug(images, bboxes, min_bboxes)
+        return images_aug, bboxes_aug, min_bboxes_aug
+
+    def apply_augmentations_ignore(
+        self, images, bboxes, min_bboxes, ignore_bboxes, min_ignore_bboxes, aug
+    ):
+        if type(images) == list:
+            images_aug = []
+            bboxes_aug = []
+            min_bboxes_aug = []
+            ignore_bboxes_aug = []
+            min_ignore_bboxes_aug = []
+
+            for image, bbox, min_bbox, ignore_bbox, min_ignore_bbox in zip(
+                images, bboxes, min_bboxes, ignore_bboxes, min_ignore_bboxes
+            ):
+                image, bbox, min_bbox, ignore_bbox, min_ignore_bbox = aug(
+                    image.unsqueeze(0),
+                    bbox.unsqueeze(0),
+                    min_bbox.unsqueeze(0),
+                    ignore_bbox.unsqueeze(0),
+                    min_ignore_bbox.unsqueeze(0),
+                )
+                images_aug.append(image.squeeze(0))
+                bboxes_aug.append(bbox.squeeze(0))
+                min_bboxes_aug.append(min_bbox.squeeze(0))
+                ignore_bboxes_aug.append(ignore_bbox.squeeze(0))
+                min_ignore_bboxes_aug.append(min_ignore_bbox.squeeze(0))
+        else:
+            (
+                images_aug,
+                bboxes_aug,
+                min_bboxes_aug,
+                ignore_bboxes_aug,
+                min_ignore_bboxes_aug,
+            ) = aug(images, bboxes, min_bboxes, ignore_bboxes, min_ignore_bboxes)
+        return (
+            images_aug,
+            bboxes_aug,
+            min_bboxes_aug,
+            ignore_bboxes_aug,
+            min_ignore_bboxes_aug,
+        )
+
     def __getitems__(self, idxs):
         batch_size = len(idxs)
 
@@ -112,70 +177,161 @@ class ICDAR2015Dataset(Dataset):
         for idx in idxs:
             bytes.append(read_file(f"{self.datadir}/images/{str(idx).zfill(6)}.jpg"))
         images = decode_jpeg(bytes, device="cuda")
-        images = torch.stack(images) / 255
 
         bboxes = [self.load_bboxes("bboxes", idx) for idx in idxs]
         num_bboxes = [len(bb) for bb in bboxes]
         bboxes = self.preprocess_bboxes(bboxes, num_bboxes, batch_size)
-
-        ignore_bboxes = [self.load_bboxes("ignore_bboxes", idx) for idx in idxs]
-        num_ignore_bboxes = [len(bb) for bb in ignore_bboxes]
-        ignore_bboxes = self.preprocess_bboxes(
-            ignore_bboxes, num_ignore_bboxes, batch_size
-        )
-
         min_bboxes = [self.load_bboxes("min_bboxes", idx) for idx in idxs]
         num_min_bboxes = [len(bb) for bb in min_bboxes]
         min_bboxes = self.preprocess_bboxes(min_bboxes, num_min_bboxes, batch_size)
 
-        min_ignore_bboxes = [self.load_bboxes("min_ignore_bboxes", idx) for idx in idxs]
-        num_min_ignore_bboxes = [len(bb) for bb in min_ignore_bboxes]
-        min_ignore_bboxes = self.preprocess_bboxes(
-            min_ignore_bboxes, num_min_ignore_bboxes, batch_size
-        )
+        if self.ignore:
+            ignore_bboxes = [self.load_bboxes("ignore_bboxes", idx) for idx in idxs]
+            num_ignore_bboxes = [len(bb) for bb in ignore_bboxes]
+            ignore_bboxes = self.preprocess_bboxes(
+                ignore_bboxes, num_ignore_bboxes, batch_size
+            )
+            min_ignore_bboxes = [
+                self.load_bboxes("min_ignore_bboxes", idx) for idx in idxs
+            ]
+            num_min_ignore_bboxes = [len(bb) for bb in min_ignore_bboxes]
+            min_ignore_bboxes = self.preprocess_bboxes(
+                min_ignore_bboxes, num_min_ignore_bboxes, batch_size
+            )
+
+        # handle images of different sizes
+        equal_size = True
+        size = images[0].shape[1:]
+        for img in images[1:]:
+            if img.shape[1:] != size:
+                equal_size = False
+                break
+        if equal_size:
+            images = torch.stack(images) / 255
+            images = self.normalize(images)
+        else:
+            images = [self.normalize(img / 255).squeeze(0) for img in images]
+            bboxes = list(bboxes)
+            min_bboxes = list(min_bboxes)
+            if self.ignore:
+                ignore_bboxes = list(ignore_bboxes)
+                min_ignore_bboxes = list(min_ignore_bboxes)
+                images, bboxes, min_bboxes, ignore_bboxes, min_ignore_bboxes = (
+                    self.apply_augmentations_ignore(
+                        images,
+                        bboxes,
+                        min_bboxes,
+                        ignore_bboxes,
+                        min_ignore_bboxes,
+                        self.upscale,
+                    )
+                )
+            else:
+                images, bboxes, min_bboxes = self.apply_augmentations(
+                    images,
+                    bboxes,
+                    min_bboxes,
+                    self.upscale,
+                )
 
         # apply augmentations
-        images = self.normalize(images)
         if self.train:
-            images, bboxes, ignore_bboxes, min_bboxes, min_ignore_bboxes = self.aug(
-                images, bboxes, ignore_bboxes, min_bboxes, min_ignore_bboxes
-            )
+            if self.ignore:
+                images, bboxes, min_bboxes, ignore_bboxes, min_ignore_bboxes = (
+                    self.apply_augmentations_ignore(
+                        images,
+                        bboxes,
+                        min_bboxes,
+                        ignore_bboxes,
+                        min_ignore_bboxes,
+                        self.aug,
+                    )
+                )
+                if type(images) == list:
+                    images = torch.stack(images)
+                    bboxes = torch.stack(bboxes)
+                    min_bboxes = torch.stack(min_bboxes)
+                    ignore_bboxes = torch.stack(ignore_bboxes)
+                    min_ignore_bboxes = torch.stack(min_ignore_bboxes)
+            else:
+                images, bboxes, min_bboxes = self.apply_augmentations(
+                    images,
+                    bboxes,
+                    min_bboxes,
+                    self.aug,
+                )
+                if type(images) == list:
+                    images = torch.stack(images)
+                    bboxes = torch.stack(bboxes)
+                    min_bboxes = torch.stack(min_bboxes)
         else:
-            h, w = images.shape[2:]
+            h, w = images[0].shape[1:]
             long_size = math.ceil((w * 640 / h) / 4) * 4
             resize = K.AugmentationSequential(
                 K.Resize((640, long_size)),
-                data_keys=["image", "keypoints", "keypoints", "keypoints", "keypoints"],
+                data_keys=self.data_keys,
                 same_on_batch=True,
             )
-            images, bboxes, ignore_bboxes, min_bboxes, min_ignore_bboxes = resize(images, bboxes, ignore_bboxes, min_bboxes, min_ignore_bboxes)
+            if self.ignore:
+                images, bboxes, min_bboxes, ignore_bboxes, min_ignore_bboxes = (
+                    self.apply_augmentations_ignore(
+                        images,
+                        bboxes,
+                        min_bboxes,
+                        ignore_bboxes,
+                        min_ignore_bboxes,
+                        resize,
+                    )
+                )
+                if type(images) == list:
+                    images = torch.stack(images)
+                    bboxes = torch.stack(bboxes)
+                    min_bboxes = torch.stack(min_bboxes)
+                    ignore_bboxes = torch.stack(ignore_bboxes)
+                    min_ignore_bboxes = torch.stack(min_ignore_bboxes)
+            else:
+                images, bboxes, min_bboxes = self.apply_augmentations(
+                    images,
+                    bboxes,
+                    min_bboxes,
+                    resize,
+                )
+                if type(images) == list:
+                    images = torch.stack(images)
+                    bboxes = torch.stack(bboxes)
+                    min_bboxes = torch.stack(min_bboxes)
 
         # process augmented bboxes
         bboxes = [
             bb[: num_bboxes[i]]
             for i, bb in enumerate(bboxes.reshape(batch_size, -1, 4, 2))
         ]
-        ignore_bboxes = [
-            bb[: num_ignore_bboxes[i]]
-            for i, bb in enumerate(ignore_bboxes.reshape(batch_size, -1, 4, 2))
-        ]
         min_bboxes = [
             bb[: num_min_bboxes[i]]
             for i, bb in enumerate(min_bboxes.reshape(batch_size, -1, 4, 2))
         ]
-        min_ignore_bboxes = [
-            bb[: num_min_ignore_bboxes[i]]
-            for i, bb in enumerate(min_ignore_bboxes.reshape(batch_size, -1, 4, 2))
-        ]
+        if self.ignore:
+            ignore_bboxes = [
+                bb[: num_ignore_bboxes[i]]
+                for i, bb in enumerate(ignore_bboxes.reshape(batch_size, -1, 4, 2))
+            ]
+            min_ignore_bboxes = [
+                bb[: num_min_ignore_bboxes[i]]
+                for i, bb in enumerate(min_ignore_bboxes.reshape(batch_size, -1, 4, 2))
+            ]
 
         # get masks
         size = images.shape[2:]
         kernel_masks, text_masks = self.get_masks(bboxes, min_bboxes, size)
-        ignore_kernel_masks, ignore_text_masks = self.get_masks(
-            ignore_bboxes, min_ignore_bboxes, size
-        )
-        ignore_kernel_masks = [1 - mask for mask in ignore_kernel_masks]
-        ignore_text_masks = [1 - mask for mask in ignore_text_masks]
+        if self.ignore:
+            ignore_kernel_masks, ignore_text_masks = self.get_masks(
+                ignore_bboxes, min_ignore_bboxes, size
+            )
+            ignore_kernel_masks = [1 - mask for mask in ignore_kernel_masks]
+            ignore_text_masks = [1 - mask for mask in ignore_text_masks]
+        else:
+            ignore_kernel_masks = [torch.ones(size, device="cuda") for _ in bboxes]
+            ignore_text_masks = [torch.ones(size, device="cuda") for _ in bboxes]
 
         return (
             list(images),
@@ -196,8 +352,7 @@ def collate_fn(batch):
 
 
 class DataLoaderIterator:
-    def __init__(self, dataset, datadir, batch_size=16, train=True):
-        dataset = dataset(datadir, train)
+    def __init__(self, dataset, batch_size=16, train=True):
         self.loader = DataLoader(
             dataset, batch_size=batch_size, shuffle=train, collate_fn=collate_fn
         )
@@ -212,11 +367,34 @@ class DataLoaderIterator:
 
 
 def get_icdar2015_loaders(root_dir, batch_size=16):
+    train_dataset = FastDataset(f"{root_dir}/icdar2015/train", train=True, ignore=True)
+    val_dataset = FastDataset(f"{root_dir}/icdar2015/val", train=False, ignore=True)
     return (
         DataLoaderIterator(
-            ICDAR2015Dataset, f"{root_dir}/icdar2015/train", batch_size, train=True
+            train_dataset,
+            batch_size,
+            train=True,
         ),
         DataLoaderIterator(
-            ICDAR2015Dataset, f"{root_dir}/icdar2015/val", batch_size, train=False
+            val_dataset,
+            batch_size,
+            train=False,
+        ),
+    )
+
+
+def get_synthtext_loaders(root_dir, batch_size=16):
+    train_dataset = FastDataset(f"{root_dir}/synthtext/train", train=True, ignore=False)
+    val_dataset = FastDataset(f"{root_dir}/synthtext/val", train=False, ignore=False)
+    return (
+        DataLoaderIterator(
+            train_dataset,
+            batch_size,
+            train=True,
+        ),
+        DataLoaderIterator(
+            val_dataset,
+            batch_size,
+            train=False,
         ),
     )
--- text-detection/process.py
+++ text-detection/process.py
@@ -8,6 +8,7 @@ import torch.nn.functional as F
 from tqdm import tqdm
 from shapely.geometry import Polygon
 from sklearn.model_selection import train_test_split
+from scipy.io import loadmat
 
 
 def create_masks(bboxes, size):
@@ -39,7 +40,7 @@ def create_masks(bboxes, size):
     return gt_kernel, gt_text
 
 
-def shrink_bboxes(bboxes):
+def get_min_bboxes(bboxes):
     rate = 0.4**2
     shrunk_bboxes = []
     for bbox in bboxes:
@@ -50,7 +51,7 @@ def shrink_bboxes(bboxes):
     return np.array(shrunk_bboxes).reshape(-1, 4, 2).astype(np.float32)
 
 
-def process_data(image_paths, gt_paths, subdir):
+def process_icdar2015_data(image_paths, gt_paths, subdir):
     os.makedirs(subdir, exist_ok=True)
     images_dir = f"{subdir}/images"
     bboxes_dir = f"{subdir}/bboxes"
@@ -65,7 +66,6 @@ def process_data(image_paths, gt_paths, subdir):
     for i, (image_path, gt_path) in tqdm(enumerate(zip(image_paths, gt_paths))):
         id = str(i).zfill(6)
         image = cv.imread(image_path)
-        # image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
         cv.imwrite(f"{images_dir}/{id}.jpg", image)
         bboxes = []
         ignore_bboxes = []
@@ -77,14 +77,17 @@ def process_data(image_paths, gt_paths, subdir):
                 else:
                     bboxes.append([gt[:8]])
         bboxes = np.array(bboxes, dtype=np.int32).reshape(-1, 4, 2)
-        min_bboxes = shrink_bboxes(bboxes)
+        min_bboxes = get_min_bboxes(bboxes)
         torch.save(torch.from_numpy(bboxes).float(), f"{bboxes_dir}/{id}.pt")
         torch.save(torch.from_numpy(min_bboxes).float(), f"{min_bboxes_dir}/{id}.pt")
         ignore_bboxes = np.array(ignore_bboxes, dtype=np.int32).reshape(-1, 4, 2)
-        min_ignore_bboxes = shrink_bboxes(ignore_bboxes)
-        torch.save(torch.from_numpy(ignore_bboxes).float(), f"{ignore_bboxes_dir}/{id}.pt")
+        min_ignore_bboxes = get_min_bboxes(ignore_bboxes)
         torch.save(
-            torch.from_numpy(min_ignore_bboxes).float(), f"{min_ignore_bboxes_dir}/{id}.pt"
+            torch.from_numpy(ignore_bboxes).float(), f"{ignore_bboxes_dir}/{id}.pt"
+        )
+        torch.save(
+            torch.from_numpy(min_ignore_bboxes).float(),
+            f"{min_ignore_bboxes_dir}/{id}.pt",
         )
 
 
@@ -99,17 +102,71 @@ def process_icdar2015(save_dir):
     train_gt_paths, val_gt_paths = train_test_split(
         gt_paths, test_size=0.2, random_state=42
     )
+
     train_subdir = f"{save_dir}/train"
     val_subdir = f"{save_dir}/val"
-    process_data(train_image_paths, train_gt_paths, train_subdir)
-    process_data(val_image_paths, val_gt_paths, val_subdir)
+
+    process_icdar2015_data(train_image_paths, train_gt_paths, train_subdir)
+    process_icdar2015_data(val_image_paths, val_gt_paths, val_subdir)
+
+
+def process_synthtext_data(image_paths, bboxes, subdir):
+    os.makedirs(subdir, exist_ok=True)
+    images_dir = f"{subdir}/images"
+    bboxes_dir = f"{subdir}/bboxes"
+    min_bboxes_dir = f"{subdir}/min_bboxes"
+    ignore_bboxes_dir = f"{subdir}/ignore_bboxes"
+    min_ignore_bboxes_dir = f"{subdir}/min_ignore_bboxes"
+    os.makedirs(images_dir, exist_ok=True)
+    os.makedirs(bboxes_dir, exist_ok=True)
+    os.makedirs(min_bboxes_dir, exist_ok=True)
+    os.makedirs(ignore_bboxes_dir, exist_ok=True)
+    os.makedirs(min_ignore_bboxes_dir, exist_ok=True)
+
+    for i, (image_path, bbox) in enumerate(tqdm(zip(image_paths, bboxes))):
+        id = str(i).zfill(6)
+        image = cv.imread(image_path)
+        cv.imwrite(f"{images_dir}/{id}.jpg", image)
+
+        min_bbox = get_min_bboxes(bbox)
+        torch.save(torch.from_numpy(bbox).float(), f"{bboxes_dir}/{id}.pt")
+        torch.save(torch.from_numpy(min_bbox).float(), f"{min_bboxes_dir}/{id}.pt")
+
+        torch.save(torch.tensor([]).reshape(0, 4, 2), f"{ignore_bboxes_dir}/{id}.pt")
+        torch.save(torch.tensor([]).reshape(0, 4, 2), f"{min_ignore_bboxes_dir}/{id}.pt")
+
+
+def process_synthtext(save_dir):
+    data = loadmat("data/raw/synthtext/gt.mat")
+    indices = list(range(data["imnames"].shape[1]))
+    train_indices, val_indices = train_test_split(
+        indices, test_size=0.1, random_state=42
+    )
+
+    train_img_paths = [
+        f"data/raw/synthtext/{data["imnames"][0, i].item()}" for i in train_indices
+    ]
+    val_img_paths = [
+        f"data/raw/synthtext/{data["imnames"][0, i].item()}" for i in val_indices
+    ]
+    train_bboxes = [data["wordBB"][0, i].T.reshape(-1, 4, 2) for i in train_indices]
+    val_bboxes = [data["wordBB"][0, i].T.reshape(-1, 4, 2) for i in val_indices]
+
+    train_subdir = f"{save_dir}/train"
+    val_subdir = f"{save_dir}/val"
+
+    # process_synthtext_data(train_img_paths, train_bboxes, train_subdir)
+    process_synthtext_data(val_img_paths, val_bboxes, val_subdir)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", "-d", type=str, required=True)
     parser.add_argument("--save", "-s", type=str, required=True)
+
     args = parser.parse_args()
+    save_dir = f"data/processed/{args.save}"
     if args.dataset == "icdar2015":
-        save_dir = f"data/processed/{args.save}"
         process_icdar2015(save_dir)
+    elif args.dataset == "synthtext":
+        process_synthtext(save_dir)
