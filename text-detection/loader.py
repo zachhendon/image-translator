Fixed augmentations and added precision/recall/f1 evaluation to the training loop

text-detection/loader.py
@@ -9,10 +9,12 @@ import math
 
 
 class FastDataset(Dataset):
-    def __init__(self, datadir, train, ignore):
+    def __init__(self, datadir, train, ignore, batch):
         self.datadir = datadir
         self.train = train
         self.ignore = ignore
+        self.batch = batch
+
         self.num_images = len(glob(f"{datadir}/images/*.jpg"))
 
         self.normalize = K.Normalize(
@@ -24,20 +26,20 @@ class FastDataset(Dataset):
         if ignore:
             self.data_keys += ["keypoints", "keypoints"]
         self.aug = K.AugmentationSequential(
-            K.RandomHorizontalFlip(p=0.5),
-            K.RandomVerticalFlip(p=0.5),
+            K.RandomGaussianBlur((3, 3), (0.1, 2.0)),
+            K.RandomSharpness(sharpness=0.1, p=1),
+            K.ColorJiggle(0.2, 0.2, 0.2, 0.2, p=1),
+            K.RandomPerspective(distortion_scale=0.1, p=1),
             K.RandomAffine(
                 degrees=(-20, 20),
-                translate=(0.2, 0.2),
-                shear=(-20, 20),
+                translate=(0.1, 0.1),
+                shear=(-5, 5),
                 scale=(0.8, 1.2),
                 p=1.0,
             ),
-            K.RandomPerspective(distortion_scale=0.3, p=0.3),
             K.RandomCrop((640, 640)),
-            K.ColorJitter(0.1, 0.1, 0.1, 0.1),
-            K.RandomGaussianBlur((3, 3), (0.1, 2.0)),
-            K.RandomSharpness(0.5),
+            K.RandomHorizontalFlip(p=0.5),
+            K.RandomVerticalFlip(p=0.5),
             K.RandomGaussianNoise(mean=0, std=0.05, p=1),
             data_keys=self.data_keys,
             same_on_batch=False,
@@ -68,7 +70,9 @@ class FastDataset(Dataset):
         bboxes = torch.stack(bboxes).reshape(batch_size, -1, 2)
         return bboxes
 
-    def get_masks(self, bboxes, min_bboxes, size):
+    def get_masks(self, images, bboxes, min_bboxes, size):
+        images_mask = images.sum(axis=1) != 0
+
         batch_bboxes = torch.vstack(bboxes)
         batch_min_bboxes = torch.vstack(min_bboxes)
         if batch_bboxes.shape[0] == 0:
@@ -104,15 +108,22 @@ class FastDataset(Dataset):
             else:
                 batch_kernel_masks.append(
                     torch.clamp(
-                        (kernel_masks[l:r] + min_kernel_masks[l:r]).sum(axis=0), 0, 1
+                        images_mask[i]
+                        * (kernel_masks[l:r] + min_kernel_masks[l:r]).sum(axis=0),
+                        0,
+                        1,
                     )
                 )
-                batch_text_masks.append(torch.clamp(text_masks[l:r].sum(axis=0), 0, 1))
+                batch_text_masks.append(
+                    images_mask[i] * torch.clamp(text_masks[l:r].sum(axis=0), 0, 1)
+                )
             l = r
         return batch_kernel_masks, batch_text_masks
 
     def apply_augmentations(self, images, bboxes, min_bboxes, aug):
-        if type(images) == list:
+        if self.batch:
+            images_aug, bboxes_aug, min_bboxes_aug = aug(images, bboxes, min_bboxes)
+        else:
             images_aug = []
             bboxes_aug = []
             min_bboxes_aug = []
@@ -121,20 +132,24 @@ class FastDataset(Dataset):
                 if len(bbox.shape) == 2:
                     bbox = bbox.unsqueeze(0)
                     min_bbox = min_bbox.unsqueeze(0)
-                image, bbox, min_bbox = aug(
-                    image, bbox, min_bbox
-                )
+                image, bbox, min_bbox = aug(image, bbox, min_bbox)
                 images_aug.append(image)
                 bboxes_aug.append(bbox)
                 min_bboxes_aug.append(min_bbox)
-        else:
-            images_aug, bboxes_aug, min_bboxes_aug = aug(images, bboxes, min_bboxes)
         return images_aug, bboxes_aug, min_bboxes_aug
 
     def apply_augmentations_ignore(
         self, images, bboxes, min_bboxes, ignore_bboxes, min_ignore_bboxes, aug
     ):
-        if type(images) == list:
+        if self.batch:
+            (
+                images_aug,
+                bboxes_aug,
+                min_bboxes_aug,
+                ignore_bboxes_aug,
+                min_ignore_bboxes_aug,
+            ) = aug(images, bboxes, min_bboxes, ignore_bboxes, min_ignore_bboxes)
+        else:
             images_aug = []
             bboxes_aug = []
             min_bboxes_aug = []
@@ -156,14 +171,6 @@ class FastDataset(Dataset):
                 min_bboxes_aug.append(min_bbox)
                 ignore_bboxes_aug.append(ignore_bbox)
                 min_ignore_bboxes_aug.append(min_ignore_bbox)
-        else:
-            (
-                images_aug,
-                bboxes_aug,
-                min_bboxes_aug,
-                ignore_bboxes_aug,
-                min_ignore_bboxes_aug,
-            ) = aug(images, bboxes, min_bboxes, ignore_bboxes, min_ignore_bboxes)
         return (
             images_aug,
             bboxes_aug,
@@ -203,17 +210,10 @@ class FastDataset(Dataset):
             )
 
         # handle images of different sizes
-        equal_size = True
-        size = images[0].shape[1:]
-        for img in images[1:]:
-            if img.shape[1:] != size:
-                equal_size = False
-                break
-        if equal_size:
+        if self.batch:
             images = torch.stack(images) / 255
-            images = self.normalize(images)
         else:
-            images = [self.normalize(img / 255) for img in images]
+            images = [img / 255 for img in images]
             bboxes = list(bboxes)
             min_bboxes = list(min_bboxes)
             if self.ignore:
@@ -250,7 +250,7 @@ class FastDataset(Dataset):
                         self.aug,
                     )
                 )
-                if type(images) == list:
+                if not self.batch:
                     images = torch.cat(images)
                     bboxes = torch.cat(bboxes)
                     min_bboxes = torch.cat(min_bboxes)
@@ -263,17 +263,17 @@ class FastDataset(Dataset):
                     min_bboxes,
                     self.aug,
                 )
-                if type(images) == list:
+                if not self.batch:
                     images = torch.cat(images)
                     bboxes = torch.cat(bboxes)
                     min_bboxes = torch.cat(min_bboxes)
         else:
-            if type(images) == list:
+            if self.batch:
+                h, w = images.shape[2:]
+            else:
                 h = sum([img.shape[2] for img in images]) // len(images)
                 w = sum([img.shape[3] for img in images]) // len(images)
-            else:
-                h, w = images[0].shape[2:]
-            
+
             long_size = math.ceil((w * 640 / h) / 4) * 4
             resize = K.AugmentationSequential(
                 K.Resize((640, long_size)),
@@ -291,7 +291,7 @@ class FastDataset(Dataset):
                         resize,
                     )
                 )
-                if type(images) == list:
+                if not self.batch:
                     images = torch.cat(images)
                     bboxes = torch.cat(bboxes)
                     min_bboxes = torch.cat(min_bboxes)
@@ -304,10 +304,14 @@ class FastDataset(Dataset):
                     min_bboxes,
                     resize,
                 )
-                if type(images) == list:
+                if not self.batch:
                     images = torch.cat(images)
                     bboxes = torch.cat(bboxes)
                     min_bboxes = torch.cat(min_bboxes)
+
+        # normalize images
+        images = self.normalize(images)
+
         # process augmented bboxes
         bboxes = [
             bb[: num_bboxes[i]]
@@ -326,36 +330,52 @@ class FastDataset(Dataset):
                 bb[: num_min_ignore_bboxes[i]]
                 for i, bb in enumerate(min_ignore_bboxes.reshape(batch_size, -1, 4, 2))
             ]
+        else:
+            ignore_bboxes = [torch.tensor([]) for _ in bboxes]
 
         # get masks
         size = images.shape[2:]
-        kernel_masks, text_masks = self.get_masks(bboxes, min_bboxes, size)
+        kernel_masks, text_masks = self.get_masks(images, bboxes, min_bboxes, size)
         if self.ignore:
             ignore_kernel_masks, ignore_text_masks = self.get_masks(
-                ignore_bboxes, min_ignore_bboxes, size
+                images, ignore_bboxes, min_ignore_bboxes, size
             )
             ignore_kernel_masks = [1 - mask for mask in ignore_kernel_masks]
             ignore_text_masks = [1 - mask for mask in ignore_text_masks]
         else:
             ignore_kernel_masks = [torch.ones(size, device="cuda") for _ in bboxes]
             ignore_text_masks = [torch.ones(size, device="cuda") for _ in bboxes]
+        images = list(images)
 
-        return (
-            list(images),
+        batch = [
+            images,
             kernel_masks,
             ignore_kernel_masks,
             text_masks,
             ignore_text_masks,
-        )
+        ]
+        if not self.train:
+            batch += [bboxes, ignore_bboxes]
+        return batch
 
 
 def collate_fn(batch):
     images = torch.stack(batch[0])
-    text_masks = torch.stack(batch[1])
-    kernel_masks = torch.stack(batch[2])
-    ignore_text_masks = torch.stack(batch[3])
-    ignore_kernel_masks = torch.stack(batch[4])
-    return images, text_masks, kernel_masks, ignore_text_masks, ignore_kernel_masks
+    kernel_masks = torch.stack(batch[1])
+    ignore_kernel_masks = torch.stack(batch[2])
+    text_masks = torch.stack(batch[3])
+    ignore_text_masks = torch.stack(batch[4])
+
+    return_batch = [
+        images,
+        kernel_masks,
+        ignore_kernel_masks,
+        text_masks,
+        ignore_text_masks,
+    ]
+    if len(batch) > 5:
+        return_batch += [batch[5], batch[6]]
+    return return_batch
 
 
 class DataLoaderIterator:
@@ -375,9 +395,11 @@ class DataLoaderIterator:
 
 def get_icdar2015_loaders(batch_size=16):
     train_dataset = FastDataset(
-        "data/processed/icdar2015/train", train=True, ignore=True
+        "data/processed/icdar2015/train", train=True, ignore=True, batch=True
     )
-    val_dataset = FastDataset("data/processed/icdar2015/val", train=False, ignore=True)
+    val_dataset = FastDataset(
+        "data/processed/icdar2015/val", train=False, ignore=True, batch=True
+    )
     return (
         DataLoaderIterator(
             train_dataset,
@@ -394,9 +416,11 @@ def get_icdar2015_loaders(batch_size=16):
 
 def get_synthtext_loaders(batch_size=16):
     train_dataset = FastDataset(
-        "data/processed/synthtext/train", train=True, ignore=False
+        "data/processed/synthtext/train", train=True, ignore=False, batch=False
     )
-    val_dataset = FastDataset("data/processed/synthtext/val", train=False, ignore=False)
+    val_dataset = FastDataset(
+        "data/processed/synthtext/val", train=False, ignore=False, batch=False
+    )
     return (
         DataLoaderIterator(
             train_dataset,

text-detection/models/utils.py
@@ -4,7 +4,6 @@ from shapely.geometry import Polygon
 import pyclipper
 import torch
 import torch.nn.functional as F
-from tqdm import tqdm
 
 
 def dilate_polygons(polygons):
@@ -61,9 +60,7 @@ def get_seg_and_poly(output_maps):
 
 
 def get_components(img):
-    # labels = []
     _, labels = cv.connectedComponents(img)
-    # labels.append(label)
     labels = np.array(labels)
     labels = torch.from_numpy(labels).to(dtype=torch.float32)
 
@@ -110,7 +107,9 @@ def get_gt_labels(bboxes, size):
     gt_labels = np.zeros(size)
 
     for i, bbox in enumerate(bboxes):
-        cv.fillPoly(gt_labels, np.expand_dims(bbox, 0).astype(np.int32), i + 1)
+        cv.fillPoly(
+            gt_labels, np.expand_dims(bbox.cpu().numpy(), 0).astype(np.int32), i + 1
+        )
 
     gt_labels = torch.from_numpy(gt_labels).cuda()
     return gt_labels
@@ -137,77 +136,8 @@ def blur_image(image):
     )
 
 
-def get_metrics(pred, bboxes):
-    # threshold = 0.75 # dice
-    threshold = 0.55  # ufl_asym
-
-    total_precision = 0.0
-    total_recall = 0.0
-    for i in range(len(pred)):
-        binary = (pred[i] > threshold).to(dtype=torch.float32)
-        # binary = (blur_image(binary) > 0.2).astype(np.uint8)
-        binary = binary.cpu().numpy().astype(np.uint8)
-        pred_labels = get_components(binary)
-        gt_labels = get_gt_labels(bboxes[i], pred_labels.shape)
-
-        pred_ids = np.unique(pred_labels.cpu())[1:].tolist()
-        gt_ids = np.unique(gt_labels.cpu())[1:].tolist()
-
-        iou_matrix = get_iou_matrix(pred_labels, gt_labels, pred_ids, gt_ids)
-        matches = match_bboxes(iou_matrix, pred_ids, gt_ids)
-
-        true_positives = len(matches)
-        false_positives = len(pred_ids) - true_positives
-        false_negatives = len(gt_ids) - true_positives
-        # return true_positives, false_positives, false_negatives
-
-        if len(gt_ids) == 0:
-            precision = 1 if len(pred_ids) == 0 else 0
-            recall = 1
-            f1 = precision
-        else:
-            precision = (
-                1
-                if true_positives + false_positives == 0
-                else true_positives / (true_positives + false_positives)
-            )
-            recall = (
-                1
-                if true_positives + false_negatives == 0
-                else true_positives / (true_positives + false_negatives)
-            )
-
-        total_precision += precision
-        total_recall += recall
-
-    return (total_precision, total_recall)
-
-
-def get_metrics_micro(pred, bboxes):
-    # threshold = 0.75 # dice
-    threshold = 0.55  # ufl_asym
-
-    for i in range(len(pred)):
-        binary = (pred[i] > threshold).to(dtype=torch.float32)
-        # binary = (blur_image(binary) > 0.2).astype(np.uint8)
-        binary = binary.cpu().numpy().astype(np.uint8)
-        pred_labels = get_components(binary)
-        gt_labels = get_gt_labels(bboxes[i], pred_labels.shape)
-
-        pred_ids = np.unique(pred_labels.cpu())[1:].tolist()
-        gt_ids = np.unique(gt_labels.cpu())[1:].tolist()
-
-        iou_matrix = get_iou_matrix(pred_labels, gt_labels, pred_ids, gt_ids)
-        matches = match_bboxes(iou_matrix, pred_ids, gt_ids)
-
-        true_positives = len(matches)
-        false_positives = len(pred_ids) - true_positives
-        false_negatives = len(gt_ids) - true_positives
-        return true_positives, false_positives, false_negatives
-
-
-def get_metrics_v2(pred, bboxes, ignore_bboxes):
-    threshold = 0.55
+def get_metrics(pred, bboxes, ignore_bboxes):
+    threshold = 0.6
 
     total_precision = 0
     total_recall = 0
@@ -254,35 +184,57 @@ def get_metrics_v2(pred, bboxes, ignore_bboxes):
     return total_precision, total_recall
 
 
-def evaluate(model, iter, limit=float("inf"), dtype=torch.float32):
+def get_metrics_micro(pred, bboxes, ignore_bboxes):
+    threshold = 0.6
+
+    total_tp = 0
+    total_fp = 0
+    total_fn = 0
+    for i in range(len(pred)):
+        binary = (pred[i] > threshold).to(dtype=torch.float32)
+        binary = binary.cpu().numpy().astype(np.uint8)
+
+        pred_labels = get_components(binary)
+        gt_labels = get_gt_labels(bboxes[i], pred_labels.shape)
+        gt_labels_ignore = get_gt_labels(ignore_bboxes[i], pred_labels.shape)
+
+        pred_ids = np.unique(pred_labels.cpu())[1:].tolist()
+        gt_ids = np.unique(gt_labels.cpu())[1:].tolist()
+        gt_ids_ignore = np.unique(gt_labels_ignore.cpu())[1:].tolist()
+
+        iou_matrix = get_iou_matrix(pred_labels, gt_labels, pred_ids, gt_ids)
+        iou_matrix_ignore = get_iou_matrix(
+            pred_labels, gt_labels_ignore, pred_ids, gt_ids_ignore
+        )
+        matches = match_bboxes(iou_matrix, pred_ids, gt_ids)
+        matches_ignore = match_bboxes(iou_matrix_ignore, pred_ids, gt_ids_ignore)
+
+        total_tp += len(matches)
+        total_fp += len(pred_ids) - len(matches) - len(matches_ignore)
+        total_fn += len(gt_ids) - len(matches)
+    return total_tp, total_fp, total_fn
+
+
+def evaluate(model, loader, iter_limit=float("inf")):
     running_precision = 0.0
     running_recall = 0.0
     dataset_size = 0
 
     model.eval()
-
     with torch.no_grad():
-        i = 0
-        while i < limit:
-            try:
-                images, (_, _, kernel_masks, text_masks, bboxes, _) = next(iter)
-            except StopIteration:
-                break
-            images = images.to(dtype=dtype, device="cuda")
-            kernel_masks = kernel_masks.to(dtype=dtype, device="cuda")
-            text_masks = text_masks.to(dtype=dtype, device="cuda")
+        iter = 0
+        while iter < iter_limit:
+            images, _, _, _, _, bboxes, ignore_bboxes = next(loader)
 
             batch_size = len(images)
 
             preds = model(images)
-            preds = preds * text_masks
-            precision, recall = get_metrics(preds, bboxes)
-            # precision, recall = get_metrics_v2(preds, bboxes, ignore_bboxes)
+            precision, recall = get_metrics(preds, bboxes, ignore_bboxes)
 
             running_precision += precision
             running_recall += recall
             dataset_size += batch_size
-            i += 1
+            iter += 1
 
     avg_precision = running_precision / dataset_size
     avg_recall = running_recall / dataset_size
@@ -290,34 +242,26 @@ def evaluate(model, iter, limit=float("inf"), dtype=torch.float32):
     return avg_precision, avg_recall, avg_f1
 
 
-def evaluate_micro(model, iter, limit=float("inf"), dtype=torch.float32):
+def evaluate_micro(model, loader, iter_limit=float("inf")):
     num_tp = 0
     num_fp = 0
     num_fn = 0
 
     model.eval()
-
     with torch.no_grad():
-        i = 0
-        while i < limit:
-            try:
-                images, (_, _, kernel_masks, text_masks, bboxes, _) = next(iter)
-            except StopIteration:
-                break
-            images = images.to(dtype=dtype, device="cuda")
-            kernel_masks = kernel_masks.to(dtype=dtype, device="cuda")
-            text_masks = text_masks.to(dtype=dtype, device="cuda")
+        iter = 0
+        while iter < iter_limit:
+            images, _, _, _, _, bboxes, ignore_bboxes = next(loader)
 
             preds = model(images)
-            preds = preds * text_masks
             true_positives, false_positives, false_negatives = get_metrics_micro(
-                preds, bboxes
+                preds, bboxes, ignore_bboxes
             )
             num_tp += true_positives
             num_fp += false_positives
             num_fn += false_negatives
 
-            i += 1
+            iter += 1
     precision = num_tp / (num_tp + num_fp)
     recall = num_tp / (num_tp + num_fn)
     f1 = 2 * precision * recall / (precision + recall)

text-detection/train.py
@@ -160,6 +160,8 @@ def main(cfg, args):
                     ignore_kernel_masks,
                     text_masks,
                     ignore_text_masks,
+                    _,
+                    _,
                 ) = next(val_loader)
 
                 preds = model(images)
@@ -182,7 +184,7 @@ def main(cfg, args):
 
         # log to tensorboard
         print(
-            f"[Iter {iteration}] | train loss: {train_loss:.4f} | val loss: {val_loss:.4f} | lr: {scheduler.get_last_lr()[0]:.7f}"
+            f"[Iter {iteration}] | train loss: {train_loss:.5f} | val loss: {val_loss:.5f} | lr: {scheduler.get_last_lr()[0]:.7f}"
         )
         writer.add_scalars(
             "kernel_loss",
@@ -211,10 +213,25 @@ def main(cfg, args):
             torch.save(checkpoint, osp.join(checkpoint_path, "best.pth"))
 
         # evaluate model
-        if int(iteration / train_eval_interval) % cfg.train.save_interval == 0:
-            pass  # TODO implement evaluate functions
-            # evaluate(model, val_loader, iteration, writer)
-            # evaluate_micro(model, val_loader, iteration, writer)
+        if iteration % cfg.train.save_interval == 0:
+            precision, recall, f1 = evaluate(model, val_loader, val_eval_interval)
+            precision_micro, recall_micro, f1_micro = evaluate_micro(
+                model, val_loader, val_eval_interval
+            )
+            print(f"precision: {precision:.4f} | recall: {recall:.4f} | f1: {f1:.4f}")
+            print(
+                f"precision_micro: {precision_micro:.4f} | recall_micro: {recall_micro:.4f} | f1_micro: {f1_micro:.4f}"
+            )
+            writer.add_scalars(
+                "metrics",
+                {"precision": precision, "recall": recall, "f1": f1},
+                iteration,
+            )
+            writer.add_scalars(
+                "metrics_micro",
+                {"precision": precision_micro, "recall": recall_micro, "f1": f1_micro},
+                iteration,
+            )
     writer.close()
 
 